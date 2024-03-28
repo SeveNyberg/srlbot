80bf73d90bbec469f6f10b3bc445a6804a053c55
SeveNyberg <shonyb@utu.fi>
2024-03-28 12:45:39 +0200

    Added new R-index fetching (originally done in bash by Aleksi Yli-Laurila) using json library in Python.

--- srlbot_plugins.py
+++ srlbot_plugins.py
@@ -1,27 +1,20 @@
 import requests
-import csv
 import numpy as np
 import time
 import datetime
+import json
 from authkey import token
 
 
 def R_index():
-    # Making a GET request
-    r = requests.get("https://space.fmi.fi/image/realtime/UT/NUR/NURdata_01.txt")
-
-    data = r.content.decode().splitlines()
-    x = list(csv.reader(data, delimiter = " "))
-    x.reverse()
-
-    Bdata = np.array([[val for val in row if val != ""] for row in x[:60]])[:,-3:].astype(float)
-
-    R_index_1 = np.abs(Bdata[:29, 0]-Bdata[1:30, 0]) + np.abs(Bdata[:29, 1]-Bdata[1:30, 1]) + np.abs(Bdata[:29, 2]-Bdata[1:30, 2])
-    R_index_2 = np.abs(Bdata[30:-1, 0]-Bdata[31:, 0]) + np.abs(Bdata[30:-1, 1]-Bdata[31:, 1]) + np.abs(Bdata[30:-1, 2]-Bdata[31:, 2])
-    R_data = [np.sum(R_index_1), np.sum(R_index_2)]
-    R_index = (R_data[0] + 2 * R_data[1])/3 /600 *1350 - 140
     
-    return R_index
+    # a la Aleksi
+    # url="https://space.fmi.fi/MIRACLE/RWC/r-index/api/NUR.json"
+    # result=$(jq '{time: .data[0].x, R: .data[0].y, curr_time: .data[0].x[-1], curr_\
+    # R: .data[0].y[-1]}' <(curl -s $url))
+    
+    r = requests.get("https://space.fmi.fi/MIRACLE/RWC/r-index/api/NUR.json")
+    return json.loads(r.content)["data"][0]["y"][-1]
 
 
 def timestamp():
